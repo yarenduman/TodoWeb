143064f53f2b6d0853803454f48b236cdb064954
Yaren Duman <yaren.duman@boun.edu.tr>
2021-06-17 11:34:46 +0300

    database updated

--- TodoWeb/Models/TodoItem.cs
+++ TodoWeb/Models/TodoItem.cs
@@ -47,6 +47,9 @@ namespace TodoWeb.Models
         public int CategoryId { get; set; }
 
         public virtual Category Category { get; set; }
+
+        public string CetUserId { get; set; }
+        public virtual CetUser CetUser { get; set; }
     }
 }
    
